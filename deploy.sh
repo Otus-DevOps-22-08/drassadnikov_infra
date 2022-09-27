11471ff40d74016fba16deb4b9ac7f2577b9c8e7
drassadnikov <drassadnikov@diasoft.ru>
2022-09-27 14:57:10 +0300

    Revert "Task 4 - drassadnikov"
    This reverts commit 2bc9d79e16bd3a0d11aeb5d7728f6fe2f1232132.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y git
-rm -rf /home/yc-user/reddit
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
-puma -d
-ps aux | grep puma
